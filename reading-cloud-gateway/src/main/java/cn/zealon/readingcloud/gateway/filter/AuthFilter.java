10fdac001249996a73894a23d8222845a91f8ab8
GitPeo <1255694270@qq.com>
2025-07-06 12:37:51 +0800

    fix:将登录、注册路由添加进白名单

--- reading-cloud-gateway/src/main/java/cn/zealon/readingcloud/gateway/filter/AuthFilter.java
+++ reading-cloud-gateway/src/main/java/cn/zealon/readingcloud/gateway/filter/AuthFilter.java
@@ -46,7 +46,8 @@ public class AuthFilter implements GlobalFilter, Ordered {
         // 主页接口、图书接口正则匹配
         boolean indexMatch = Pattern.matches("/index[^\\s]*", path);
         boolean bookMatch = Pattern.matches("/book/[^\\s]*", path);
-
+        whiteList.add("/account/user/login");
+        whiteList.add("/account/user/register");
         // 白名单接口、开放接口放行
         if (bookMatch || indexMatch || whiteList.contains(path)) {
             return chain.filter(exchange);
